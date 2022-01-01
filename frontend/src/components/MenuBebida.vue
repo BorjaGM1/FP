30bcca7e2f91e3928c61bc4dee6e2f8170350a74
borji <borjitaea@gmail.com>
2022-01-01 13:55:30 +0100

    First

---
+++ frontend/src/components/MenuBebida.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <button @click="elegir_bebida('Cola')">Cola</button>
+  <button @click="elegir_bebida('Limon')">Limon</button>
+  <button @click="elegir_bebida('Naranja')">Naranja</button>
+  <br/>
+
+  <button @click="atras()">Atras</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MenuBebida",
+  methods: {
+    elegir_bebida: function (tipo){
+      this.$emit("nueva-bebida",tipo)
+    },
+    atras: function() {
+      this.$emit("atras")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Pedido.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  Su pedido actual:
+  <li v-for="elemento in pedido" :key="elemento">
+    {{elemento}}
+  </li>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Pedido",
+  props: {pedido: Array,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
